994236f6a51ff6dd90844fd1178d8adf23d64406
tools.roysmith-test <tools.roysmith-test@tools-sgebastion-07.tools.eqiad.wmflabs>
2019-10-09 17:16:18 +0000

    Start exploring categories.

--- src/user_profile/tests.py
+++ src/user_profile/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+from . import views
+from unittest import TestCase
+from unittest.mock import MagicMock
 
-# Create your tests here.
+
+class ViewTestCase(TestCase):
+    def test_get_category_tree(self):
+        values = {
+            'Margaret Sibella Brown': {'c1', 'c2'},
+            'c1': {'c3'},
+            }
+        views._get_page_categories = MagicMock(side_effect=lambda x: values.get(x, set()))
+        categories = views._get_category_tree('Margaret Sibella Brown', 3)
+        self.assertEqual(categories, {'c1', 'c2', 'c3'})
+        
--- src/user_profile/views.py
+++ src/user_profile/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
+from mwclient import Site
 
 from .forms import GetPageTitleForm
 
@@ -21,10 +22,29 @@ def get_page_title(request):
     if request.method == 'POST':
         form = GetPageTitleForm(request.POST)
         if form.is_valid():
-            context = {'title': form.cleaned_data['page_title']}
+            page_title = form.cleaned_data['page_title']
+            categories = _get_category_tree(page_title, 3)
+            context = {'title': page_title, 'categories': categories}
             return render(request, 'user_profile/page_title.dtl', context)
     else:
         form = GetPageTitleForm()
     context = {'form': form} 
     return render(request, 'user_profile/get_page_title.dtl', context)
 
+
+def _get_category_tree(page_title, depth):
+    categories = _get_page_categories(page_title)
+    subcats = set()
+    if depth > 1:
+        for cat in categories:
+            subcats.update(_get_category_tree(cat, depth-1))
+    return categories | subcats
+            
+    
+
+def _get_page_categories(page_title):
+    """Return a set of the names of the categories this page belongs to."""
+    ua = "CheckRefs/0.0 (User:RoySmith)"
+    site = Site('en.wikipedia.org', clients_useragent=ua)
+    page = site.pages[page_title].resolve_redirect()
+    return {cat.name for cat in page.categories()}
