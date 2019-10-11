fd9f5ca7138cc6ccb3444ef13e048b2a2ee58236
tools.roysmith-test <tools.roysmith-test@tools-sgebastion-07.tools.eqiad.wmflabs>
2019-10-11 00:37:16 +0000

    Refactor, and add more tests.

--- user_profile/tests.py
+++ user_profile/tests.py
@@ -4,12 +4,25 @@ from unittest.mock import MagicMock
 
 
 class ViewTestCase(TestCase):
-    def test_get_category_tree(self):
-        values = {
-            'Margaret Sibella Brown': {'c1', 'c2'},
-            'c1': {'c3'},
-            }
-        views._get_page_categories = MagicMock(side_effect=lambda x: values.get(x, set()))
-        categories = views._get_category_tree('Margaret Sibella Brown', 3)
-        self.assertEqual(categories, {'c1', 'c2', 'c3'})
-        
+    def install_mock(self, value_dict):
+        """Installs a MagicMock object configured to return specific values,
+        held in values_dict; this is a mapping from arguments to return values.
+        """
+        views._get_category_names = MagicMock(
+            side_effect=lambda x: value_dict.get(x, set()))
+
+    def test_get_categories_no_parents(self):
+        self.install_mock({
+            'page': {'c1', 'c2'},
+        })
+        self.assertEqual(views._get_categories('page', 3),
+                         {views.CategoryGraph('c1'),
+                          views.CategoryGraph('c2')
+                         })
+
+    def test_get_categories_empty_set(self):
+        self.install_mock({
+            'page': {},
+        })
+        self.assertEqual(views._get_categories('page', 3),
+                         set())
--- user_profile/views.py
+++ user_profile/views.py
@@ -23,7 +23,7 @@ def get_page_title(request):
         form = GetPageTitleForm(request.POST)
         if form.is_valid():
             page_title = form.cleaned_data['page_title']
-            categories = _get_category_tree(page_title, 3)
+            categories = _get_categories(page_title, 3)
             context = {'title': page_title, 'categories': categories}
             return render(request, 'user_profile/page_title.dtl', context)
     else:
@@ -32,19 +32,43 @@ def get_page_title(request):
     return render(request, 'user_profile/get_page_title.dtl', context)
 
 
-def _get_category_tree(page_title, depth):
-    categories = _get_page_categories(page_title)
-    subcats = set()
-    if depth > 1:
-        for cat in categories:
-            subcats.update(_get_category_tree(cat, depth-1))
-    return categories | subcats
-            
+def _get_categories(page_title, depth):
+    """Return a set of CategoryGraphs for the given page.
+    The category graph will be navigated to the specified depth."""
+    category_names = _get_category_names(page_title)
+    categories = set()
+    for name in category_names:
+        categories.add(CategoryGraph(name))
+    return categories
+        
     
+#    if depth > 1:
+#        for cat in categories:
+#            subcats.update(_get_category_graph(cat, depth-1))
+#    return cats
+            
 
-def _get_page_categories(page_title):
+def _get_category_names(page_title):
     """Return a set of the names of the categories this page belongs to."""
     ua = "CheckRefs/0.0 (User:RoySmith)"
     site = Site('en.wikipedia.org', clients_useragent=ua)
     page = site.pages[page_title].resolve_redirect()
     return {cat.name for cat in page.categories()}
+
+
+class CategoryGraph:
+    def __init__(self, name):
+        self.name = name
+        self.parents = {}
+
+    def __iter__(self):
+        for parent in self.parents:
+            yield parent
+
+    def __eq__(self, other):
+        return self.name == other.name and self.parents == other.parents
+
+    def __hash__(self):
+        # Hashing just the name is rather minimal, but simple, and
+        # probably good enough for our purposes.
+        return hash(self.name)
