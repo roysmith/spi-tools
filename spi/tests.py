36ac21246ac7ad17364f21dd50eb5893957fd9e2
tools.roysmith-test <tools.roysmith-test@tools-sgebastion-07.tools.eqiad.wmflabs>
2019-10-25 13:30:19 +0000

    Add SPIArchive class.

--- spi/tests.py
+++ spi/tests.py
@@ -1,3 +1,34 @@
+import os
 from django.test import TestCase
 
-# Create your tests here.
+from . import views
+
+
+class SPIArchiveTestCase(TestCase):
+    def test_get_master_name(self):
+        text = '''
+        {{SPIarchive notice|1=KaranSharma0445}}
+        '''
+        archive = views.SPIArchive(text)
+        expected =  'KaranSharma0445'
+        self.assertEqual(archive.master_name(), expected)
+
+
+    def test_get_socks(self):
+        text = '''
+        * {{checkuser|1=Sharvind Page}}
+        * {{checkuser|1=PoSharvind}}
+        * {{checkIP|86.170.34.216}}
+        '''
+        archive = views.SPIArchive(text)
+        expected =  {'Sharvind Page', 'PoSharvind', '86.170.34.216'}
+        self.assertEqual(archive.socks(), expected)
+
+
+    def test_get_socks_extra_whitespace(self):
+        text = '''
+        * {{checkuser|1=DipikaKakar346 }}
+        '''
+        archive = views.SPIArchive(text)
+        expected =  {'DipikaKakar346'}
+        self.assertEqual(archive.socks(), expected)
--- spi/views.py
+++ spi/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from mwclient import Site
+import mwparserfromhell
+
 from .forms import SummaryForm
+
+
+SITE_NAME = 'en.wikipedia.org'
+
 
 def index(request):
     context = {}
     return render(request, 'spi/index.dtl', context)
+
 
 def summary(request):
     if request.method == 'POST':
@@ -13,10 +20,36 @@ def summary(request):
             master_name = form.cleaned_data['master_name']
             archive_page_title = 'Wikipedia:Sockpuppet investigations/%s/Archive' % master_name
             context = {'form': form,
-                       'info': archive_page_title,
+                       'info': _get_wikitext(archive_page_title),
             }
             return render(request, 'spi/summary.dtl', context)
     else:
         form = SummaryForm()
     context = {'form': form} 
     return render(request, 'spi/summary.dtl', context)
+
+
+def _get_wikitext(page_title):
+    site = Site(SITE_NAME)
+    page = site.pages[page_title]
+    return page.text()
+
+
+class SPIArchive:
+    def __init__(self, text):
+        self.text = text
+        self.code = mwparserfromhell.parse(text)
+
+    def master_name(self):
+        templates = self.code.filter_templates(
+            matches = lambda n: n.name.matches('SPIarchive notice'))
+        n = len(templates)
+        if n != 1:
+            raise ValueError("wrong number of SPIarchive notices: expected 1, got %s" % n)
+        return templates[0].get('1').value
+
+
+    def socks(self):
+        templates = self.code.filter_templates(
+            matches = lambda n: n.name.matches('checkuser') or n.name.matches('checkIP'))
+        return set(t.get('1').value.strip_code().strip() for t in templates)
