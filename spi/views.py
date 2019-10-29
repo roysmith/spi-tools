2e255f7bb398593d8ba645e10512acc5df89095a
tools.roysmith-test <tools.roysmith-test@tools-sgebastion-07.tools.eqiad.wmflabs>
2019-10-29 01:14:33 +0000

    Add SockInfo.

--- spi/views.py
+++ spi/views.py
@@ -1,3 +1,4 @@
+from collections import namedtuple
 from django.shortcuts import render
 from mwclient import Site
 import mwparserfromhell
@@ -18,9 +19,8 @@ def summary(request):
         form = SummaryForm(request.POST)
         if form.is_valid():
             master_name = form.cleaned_data['master_name']
-            archive_page_title = 'Wikipedia:Sockpuppet investigations/%s/Archive' % master_name
             context = {'form': form,
-                       'info': _get_wikitext(archive_page_title),
+                       'info': _find_socks(master_name),
             }
             return render(request, 'spi/summary.dtl', context)
     else:
@@ -29,16 +29,26 @@ def summary(request):
     return render(request, 'spi/summary.dtl', context)
 
 
-def _get_wikitext(page_title):
+def _find_socks(master_name):
+     """Returns a list of User tuples."""
+     archive_page_title = 'Wikipedia:Sockpuppet investigations/%s/Archive' % master_name
+     case = SPICase(_get_wiki_text(archive_page_title))
+     return case.get_users()
+
+
+def _get_wiki_text(page_title):
     site = Site(SITE_NAME)
     page = site.pages[page_title]
     return page.text()
 
 
+SockInfo = namedtuple('SockInfo', 'username, blocked')
+
 class SPICase:
     def __init__(self, text):
         self.text = text
         self.code = mwparserfromhell.parse(text)
+
 
     def master_name(self):
         """Return the name of the sockmaster, parsed from a {{SPIarchive notice}}
@@ -53,6 +63,15 @@ class SPICase:
 
 
     def socks(self):
+        """Return a set of usernames of suspected socks."""
         templates = self.code.filter_templates(
             matches = lambda n: n.name.matches('checkuser') or n.name.matches('checkIP'))
         return set(t.get('1').value.strip_code().strip() for t in templates)
+
+
+    def get_users(self):
+        socks=self.socks()
+        site = Site(SITE_NAME)
+        wiki_users = site.users(socks)
+        return [SockInfo(w['name'], 'blockid' in w) for w in wiki_users]
+
