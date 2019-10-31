a34f6b9c96142fa58b18626cce4f95f4a55b0b02
tools.roysmith-test <tools.roysmith-test@tools-sgebastion-07.tools.eqiad.wmflabs>
2019-10-31 17:43:07 +0000

    Refactor test to return datafile contents, not a file handle.

--- spi/spi_utils_test.py
+++ spi/spi_utils_test.py
@@ -5,11 +5,12 @@ from spi_utils import SPICase, ArchiveError
 
 
 class SPICaseTest(TestCase):
-    def datafile(self, filename):
-        """Open a data file and Return a read-only file object"""
+    def read_data_file(self, filename):
+        """Return the contents of a data file"""
         dirname = os.path.dirname(__file__) 
         path = os.path.join(dirname, 'test-data', filename)
-        return open(path, 'r')
+        with open(path, 'r') as f:
+            return f.read()
     
 
     def test_wikitext_is_stored(self):
@@ -48,8 +49,5 @@ class SPICaseTest(TestCase):
 
 
     def test_dates_returns_correct_date(self):
-        with self.datafile('spi-simmerdon3448') as datafile:
-            case = SPICase(datafile)
-            self.assertEqual(case.dates(), ['30 October 2019'])
-
-        
+        case = SPICase(self.read_data_file('spi-simmerdon3448'))
+        self.assertEqual(case.dates(), ['30 October 2019'])
