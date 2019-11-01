a362179186ed20cef905397bc72357b1bfdbf66d
tools.roysmith-test <tools.roysmith-test@tools-sgebastion-07.tools.eqiad.wmflabs>
2019-11-01 01:11:40 +0000

    Add SPICaseDay class and related tests.

--- spi/spi_utils.py
+++ spi/spi_utils.py
@@ -1,4 +1,9 @@
+from collections import namedtuple
 import mwparserfromhell
+
+SPICheckUser = namedtuple('SPICheckUser', 'username, date')
+SPICheckIP = namedtuple('SPICheckIP', 'ip, date')
+
 
 class ArchiveError(ValueError):
     pass
@@ -18,16 +23,51 @@ class SPICase:
         templates = self.wikicode.filter_templates(
             matches = lambda n: n.name.matches('SPIarchive notice'))
         n = len(templates)
-        if n ==  1:
+        if n == 1:
             return templates[0].get('1').value
         raise ArchiveError("Expected exactly 1 {{SPIarchive notice}}, found %d" % n)
 
 
-    def dates(self):
-        """Return a list of date strings.
+    def days(self):
+        """Return an iterable of SPICaseDays"""
+        sections = self.wikicode.get_sections(levels=[3])
+        return [SPICaseDay(s) for s in sections]
 
-        It is assumed that all level-3 headings are dates.
-        """
+
+class SPICaseDay:
+    def __init__(self, wikicode):
+        self.wikicode = wikicode
+
+
+    def date(self):
         headings = self.wikicode.filter_headings(matches = lambda h: h.level == 3)
-        dates = [h.title for h in headings]
-        return dates
+        n = len(headings)
+        if n == 1:
+            return headings[0].title
+        raise ArchiveError("Expected exactly 1 level-3 heading, found %d" % n)
+
+
+    def find_users(self):
+        '''Iterates over all the accounts mentioned in checkuser templates.
+        Each user is represented as a SpiCheckUser tuple.  Order of iteration
+        is not guaranteed, and templates are not deduplicated.
+        '''
+        date = self.date()
+        templates = self.wikicode.filter_templates(
+            matches = lambda n: n.name.matches('checkuser'))
+        for t in templates:
+            username = t.get('1').value
+            yield SPICheckUser(username, date)
+
+
+    def find_ips(self):
+        '''Iterates over all the IPs mentioned in checkuser templates.
+        Each user is represented as a SpiCheckIP tuple.  Order of iteration
+        is not guaranteed, and templates are not deduplicated.
+        '''
+        date = self.date()
+        templates = self.wikicode.filter_templates(
+            matches = lambda n: n.name.matches('checkip'))
+        for t in templates:
+            ip = t.get('1').value
+            yield SPICheckIP(ip, date)
--- spi/spi_utils_test.py
+++ spi/spi_utils_test.py
@@ -1,18 +1,16 @@
 from unittest import TestCase
+import textwrap
 import os.path
+import mwparserfromhell
 
-from spi_utils import SPICase, ArchiveError
+from spi_utils import SPICase, SPICaseDay, SPICheckUser, SPICheckIP, ArchiveError
+
+
+def make_code(text):
+    return mwparserfromhell.parse(textwrap.dedent(text))
 
 
 class SPICaseTest(TestCase):
-    def read_data_file(self, filename):
-        """Return the contents of a data file"""
-        dirname = os.path.dirname(__file__) 
-        path = os.path.join(dirname, 'test-data', filename)
-        with open(path, 'r') as f:
-            return f.read()
-    
-
     def test_wikitext_is_stored(self):
         text = '''
         foo
@@ -48,6 +46,80 @@ class SPICaseTest(TestCase):
             case.master_name()
 
 
-    def test_dates_returns_correct_date(self):
-        case = SPICase(self.read_data_file('spi-simmerdon3448'))
-        self.assertEqual(case.dates(), ['30 October 2019'])
+    def test_days_returns_iterable_of_case_days(self):
+        text = '''
+        __TOC__
+        {{SPIarchive notice|1=Crazyalien}}
+        {{SPIpriorcases}}
+        ===21 March 2019===
+        ====Suspected sockpuppets====
+        ===22 May 2019===
+        ====Suspected sockpuppets====
+        ===13 July 2019===
+        ====Suspected sockpuppets====
+        '''
+        case = SPICase(text)
+        for d in case.days():
+            self.assertIsInstance(d, SPICaseDay)
+
+
+class SPICaseDayTest(TestCase):
+    def test_date_returns_correct_date(self):
+        text = '''
+        {{SPIarchive notice|1=Crazyalien}}
+        {{SPIpriorcases}}
+        ===21 March 2019===
+        blah, blah, blaha
+        '''
+        day = SPICaseDay(make_code(text))
+        date = day.date()
+        self.assertEqual(date, '21 March 2019')
+
+
+    def test_day_with_multiple_level_3_headers_raises_archive_error(self):
+        text = '''
+        {{SPIarchive notice|1=Crazyalien}}
+        {{SPIpriorcases}}
+        ===21 March 2019===
+        ===22 March 2019===
+        blah, blah, blaha
+        '''
+        day = SPICaseDay(make_code(text))
+        with self.assertRaises(ArchiveError):
+            day.date()
+
+
+    def test_day_with_no_level_3_headers_raises_archive_error(self):
+        text = '''
+        {{SPIarchive notice|1=Crazyalien}}
+        {{SPIpriorcases}}
+        blah, blah, blaha
+        '''
+        day = SPICaseDay(make_code(text))
+        with self.assertRaises(ArchiveError):
+            day.date()
+
+
+    def test_find_users(self):
+        text = '''
+        ===21 March 2019===
+        {{checkuser|user1}}
+        {{checkuser|user2}}
+        '''
+        day = SPICaseDay(make_code(text))
+        users = list(day.find_users())
+        self.assertEqual(users, [SPICheckUser('user1', '21 March 2019'),
+                                 SPICheckUser('user2', '21 March 2019')])
+
+
+    def test_find_ips(self):
+        text = '''
+        ===21 March 2019===
+        {{checkip|1.2.3.4}}
+        {{checkip|5.6.7.8}}
+        '''
+        day = SPICaseDay(make_code(text))
+        ips = list(day.find_ips())
+        self.assertEqual(ips, [SPICheckIP('1.2.3.4', '21 March 2019'),
+                               SPICheckIP('5.6.7.8', '21 March 2019')])
+
