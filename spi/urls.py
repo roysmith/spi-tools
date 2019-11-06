35f1ebf33ed6b8f910738553c23c6df22e777498
tools.roysmith-test <tools.roysmith-test@tools-sgebastion-07.tools.eqiad.wmflabs>
2019-11-06 01:48:53 +0000

    Tore out obsolete forms processing behind the server-side block range code.

--- spi/urls.py
+++ spi/urls.py
@@ -4,5 +4,4 @@ from spi import views
 urlpatterns = [
     path('', views.index, name="spi-index"),
     path('ip-analysis/<case_name>/', views.ip_analysis, name="spi-ip-analysis"),
-    path('ip-range/<case_name>/<first_ip>/<last_ip>/', views.ip_range, name='spi-ip-range'),
 ]
--- spi/views.py
+++ spi/views.py
@@ -24,28 +24,11 @@ def index(request):
 
 
 def ip_analysis(request, case_name):
-    if request.method == 'POST':
-        form = IpRangeForm(request.POST)
-        if form.is_valid():
-            first_ip = form.cleaned_data['first_ip']
-            last_ip = form.cleaned_data['last_ip']
-            return redirect('spi-ip-range', case_name, first_ip, last_ip)
-        else:
-            form = IpRangeForm()
     infos = sorted(get_spi_case_ips(case_name))
     context = {'case_name': case_name,
                'ip_infos': infos,
     }
     return render(request, 'spi/ip-analysis.dtl', context)
-
-def ip_range(request, case_name, first_ip, last_ip):
-    infos = [SpiIpInfo(first_ip, ''),
-             SpiIpInfo(last_ip, '')]
-    network = SpiIpInfo.find_common_network(infos)
-    context = {'case_name': case_name,
-               'network': network,
-    }
-    return render(request, 'spi/ip-range.dtl', context)
 
 
 def get_spi_case_ips(master_name):
