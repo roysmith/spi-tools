37a4eb453ab8f6445ff326acb7317bb39d80be7b
Roy Smith <roy@panix.com>
2020-01-12 18:05:56 +0000

    Be consistent in use of single vs double quotes.

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ from pathlib import Path
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tools_app.settings")
-os.environ.setdefault('DJANGO_SECRET', (Path.home() / 'www/python/secret').read_text().strip())
+os.environ.setdefault("DJANGO_SECRET", (Path.home() / "www/python/secret").read_text().strip())
 
 app = get_wsgi_application()
