b291a3bca9c53d9a71288172a3599bcb104e87cb
Roy Smith <roy@panix.com>
2020-01-13 14:54:43 +0000

    Remove max_length parameter from case_name field.

--- spi/forms.py
+++ spi/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 class CaseNameForm(forms.Form):
-    case_name = forms.CharField(label='Case (sockmaster) name', max_length=100)
+    case_name = forms.CharField(label='Case (sockmaster) name')
     use_archive = forms.BooleanField(label='Use archive?', required=False)
 
 class IpRangeForm(forms.Form):
