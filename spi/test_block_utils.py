701143774ff5278d78f47254a5e5570018f16d7a
Roy Smith <roy@panix.com>
2020-07-26 02:08:28 +0000

    Lint: longer function names.

--- spi/test_block_utils.py
+++ spi/test_block_utils.py
@@ -10,25 +10,25 @@ from .block_utils import Block, BlockMap
 
 max_utc = datetime.replace(datetime.max, tzinfo=timezone.utc)
 
-def st(year, month, day):
+def _st(year, month, day):
     "Construct a struct_time."
     return (year, month, day, 0, 0, 0, 0, 0, 0)
 
-def dt(year, month, day):
+def _dt(year, month, day):
     "Construct a UTC-aware datetime."
     return datetime(year, month, day, 0, 0, 0, tzinfo=timezone.utc)
 
 
 class BlockTest(TestCase):
     def test_construct_indef_block(self):
-        block = Block(st(2020, 2, 7), 'infinity')
-        self.assertEqual(block.start, dt(2020, 2, 7))
+        block = Block(_st(2020, 2, 7), 'infinity')
+        self.assertEqual(block.start, _dt(2020, 2, 7))
         self.assertEqual(block.end, max_utc)
 
     def test_construct_finite_block(self):
-        block = Block(st(2020, 2, 7), st(2020, 4, 1))
-        self.assertEqual(block.start, dt(2020, 2, 7))
-        self.assertEqual(block.end, dt(2020, 4, 1))
+        block = Block(_st(2020, 2, 7), _st(2020, 4, 1))
+        self.assertEqual(block.start, _dt(2020, 2, 7))
+        self.assertEqual(block.end, _dt(2020, 4, 1))
 
 
 class BlockMapTest(TestCase):
@@ -38,21 +38,21 @@ class BlockMapTest(TestCase):
 
 
     def test_construct_with_one_block(self):
-        api_block = {'timestamp': st(2020, 2, 7),
-                     'expiry': st(2020, 4, 1)}
+        api_block = {'timestamp': _st(2020, 2, 7),
+                     'expiry': _st(2020, 4, 1)}
         block_map = BlockMap([api_block])
         self.assertEqual(len(block_map.blocks), 1)
-        self.assertEqual(block_map.blocks[0].start, dt(2020, 2, 7))
-        self.assertEqual(block_map.blocks[0].end, dt(2020, 4, 1))
+        self.assertEqual(block_map.blocks[0].start, _dt(2020, 2, 7))
+        self.assertEqual(block_map.blocks[0].end, _dt(2020, 4, 1))
 
 
     def test_is_blocked_at_time(self):
-        api_blocks = [{'timestamp': st(2020, 2, 7), 'expiry': st(2020, 4, 1)},
-                      {'timestamp': st(2020, 6, 1), 'expiry': 'infinity'},
+        api_blocks = [{'timestamp': _st(2020, 2, 7), 'expiry': _st(2020, 4, 1)},
+                      {'timestamp': _st(2020, 6, 1), 'expiry': 'infinity'},
                       ]
         block_map = BlockMap(api_blocks)
-        self.assertFalse(block_map.is_blocked_at(dt(2020, 2, 6)))
-        self.assertTrue(block_map.is_blocked_at(dt(2020, 2, 8)))
-        self.assertFalse(block_map.is_blocked_at(dt(2020, 4, 2)))
-        self.assertTrue(block_map.is_blocked_at(dt(2020, 6, 2)))
-        self.assertTrue(block_map.is_blocked_at(dt(3000, 1, 1)))
+        self.assertFalse(block_map.is_blocked_at(_dt(2020, 2, 6)))
+        self.assertTrue(block_map.is_blocked_at(_dt(2020, 2, 8)))
+        self.assertFalse(block_map.is_blocked_at(_dt(2020, 4, 2)))
+        self.assertTrue(block_map.is_blocked_at(_dt(2020, 6, 2)))
+        self.assertTrue(block_map.is_blocked_at(_dt(3000, 1, 1)))
