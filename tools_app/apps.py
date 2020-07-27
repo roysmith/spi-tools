629b084829f4fb22d9d78c9534ec96c024265a91
Roy Smith <roy@panix.com>
2020-07-27 16:55:40 +0000

    Don't log startup when running under manage.py (i.e. in testing).

--- tools_app/apps.py
+++ tools_app/apps.py
@@ -1,4 +1,5 @@
 import logging
+import sys
 from django.apps import AppConfig
 
 
@@ -9,4 +10,5 @@ class ToolsAppConfig(AppConfig):
     name = 'tools_app'
 
     def ready(self):
-        logger.critical(f'{self.name} starting')
+        if 'manage.py' not in sys.argv[0]:
+            logger.critical(f'{self.name} starting (argv = {sys.argv})')
