399c530fe24cdf55690f93f995412e2742da0eee
Roy Smith <roy@panix.com>
2020-07-28 22:23:55 +0000

    Add basic SockInfoView test.
    This is mostly a proof-of-concept for how the django test client and
    unittest.mock work together.

--- spi/test_views.py
+++ spi/test_views.py
@@ -1,4 +1,8 @@
 from unittest import TestCase
+from unittest.mock import patch, MagicMock
+
+from django.test import Client
+
 from .views import SockSelectView, UserSummary
 from .spi_utils import SpiUserInfo
 
@@ -44,3 +48,17 @@ class UserSummaryTest(TestCase):
         summary = UserSummary('foo/bar', '20 July 2020')
         self.assertEqual(summary.username, 'foo/bar')
         self.assertEqual(summary.urlencoded_username(), 'foo%2Fbar')
+
+
+class SockInfoViewTest(TestCase):
+    @patch('mwclient.Site', new_callable=MagicMock, spec=['pages', 'users'])
+    def test_get_with_empty_mw_queries_renders_one_summary(self, mock_Site):
+        mock_site = mock_Site()
+        mock_site.pages.__getitem__().text.return_value = ''
+        mock_site.users().return_value = iter([{}])
+        client = Client()
+        response = client.get('/spi/spi-sock-info/Foo/')
+        self.assertIn('spi/sock-info.dtl', [t.name for t in response.templates])
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context['case_name'], 'Foo')
+        self.assertEqual(response.context['summaries'], [UserSummary('Foo', None)])
