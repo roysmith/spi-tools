3aca3994875da1684994aa4b2fd2b528cf97a070
Roy Smith <roy@panix.com>
2020-08-29 00:24:06 +0000

    Add some more forms unit tests.
    See #102.

--- spi/test_forms.py
+++ spi/test_forms.py
@@ -1,9 +1,11 @@
+from unittest.mock import patch
 from unittest import TestCase
 import urllib.parse
 
 from django.forms import BooleanField
 
-from spi.forms import SockSelectForm
+from spi.forms import CaseNameForm, SockSelectForm
+
 
 class SockSelectFormTest(TestCase):
     def test_build_with_simple_names(self):
@@ -18,3 +20,34 @@ class SockSelectFormTest(TestCase):
         form = SockSelectForm.build([name])
         self.assertIsInstance(form, SockSelectForm)
         self.assertIsInstance(form.fields['sock_' + quoted_name], BooleanField)
+
+
+class CaseNameFormTest(TestCase):
+    # pylint: disable=invalid-name
+
+    @patch('spi.forms.Wiki')
+    def test_validate_with_valid_case_name(self, mock_Wiki):
+        mock_Wiki().page_exists.return_value = True
+        data = {'case_name': 'Fred',
+                'use_archive': True,
+                }
+
+        form = CaseNameForm(data)
+
+        self.assertTrue(form.is_valid())
+
+
+    @patch('spi.forms.Wiki')
+    def test_validate_with_invalid_case_name(self, mock_Wiki):
+        mock_Wiki().page_exists.return_value = False
+        data = {'case_name': 'Fred',
+                'use_archive': True,
+                }
+
+        form = CaseNameForm(data)
+
+        self.assertFalse(form.is_valid())
+        errors = form.errors.as_data()
+        self.assertCountEqual(errors.keys(), ['case_name'])
+        self.assertEqual(len(errors['case_name']), 1)
+        self.assertEqual(errors['case_name'][0].code, 'invalid_choice')
