a38b5ab5e7336cebef075ce13b742aa056365922
Roy Smith <roy@panix.com>
2021-01-16 23:18:59 +0000

    Delint: fold long line.

--- spi/test_views.py
+++ spi/test_views.py
@@ -88,7 +88,9 @@ class IndexViewTest(TestCase):
         self.assertEqual(response.status_code, 200)
         data = json.loads(response.context['choices'])
         expected_data = [{'id': '', 'text': ''},
-                         {'id': 'Adhithya Kiran Chekavar', 'text': 'Adhithya Kiran Chekavar', 'selected': True}]
+                         {'id': 'Adhithya Kiran Chekavar',
+                          'text': 'Adhithya Kiran Chekavar',
+                          'selected': True}]
         self.assertEqual(data, expected_data)
 
 
