fd35240624d951f61b372320914ac0f724f29691
Roy Smith <roy@panix.com>
2022-01-16 04:31:41 +0000

    Add Category support

--- spi/test_get_sock_names.py
+++ spi/test_get_sock_names.py
@@ -1,13 +1,24 @@
-from unittest.mock import patch, NonCallableMock
+from unittest.mock import patch, call, MagicMock, NonCallableMock
 
 from django.test import TestCase
 
-from spi.views import get_sock_names, ValidatedUser
+from spi.views import get_sock_names, ValidatedUser, SockType
 from spi.spi_utils import CacheableSpiCase, SpiUserInfo
-from wiki_interface import Wiki
+from wiki_interface import Wiki, Category
 
 
 # pylint: disable=invalid-name
+
+
+class ValidatedUserTest(TestCase):
+    def test_default_sock_type_is_none(self):
+        user = ValidatedUser('foo', '1 January 2020', True)
+        self.assertEqual(user.sock_type, SockType.NONE)
+
+
+    def test_sock_type_can_be_set(self):
+        user = ValidatedUser('foo', '1 January 2020', True, SockType.KNOWN)
+        self.assertEqual(user.sock_type, SockType.KNOWN)
 
 
 class GetSockNamesTest(TestCase):
@@ -29,6 +40,7 @@ class GetSockNamesTest(TestCase):
         mock_CacheableSpiCase.get.return_value = CacheableSpiCase('fred', 1, [], [])
         wiki = NonCallableMock(Wiki)
         wiki.validate_usernames.return_value = {}
+        wiki.category().members.return_value = []
         sock_names = get_sock_names(wiki, 'fred')
         self.assertEqual(sock_names, [])
 
@@ -46,9 +58,56 @@ class GetSockNamesTest(TestCase):
                                                                   [])
         wiki = NonCallableMock(Wiki)
         wiki.validate_usernames.return_value = {'1.2.3.0/24'}
+        wiki.category().members.return_value = []
         socks = get_sock_names(wiki, 'fred')
         wiki.validate_usernames.assert_called_once_with(['sock1', 'sock2', '1.2.3.4', '1.2.3.0/24'])
         self.assertCountEqual(socks, [ValidatedUser('sock1', '1 January 2020', True),
                                       ValidatedUser('sock2', '1 January 2020', True),
                                       ValidatedUser('1.2.3.4', '1 January 2020', True),
                                       ValidatedUser('1.2.3.0/24', '1 January 2020', False)])
+
+
+    @patch('spi.views.CacheableSpiCase')
+    @patch('spi.views.cache')
+    def test_get_sock_names_retrieves_sock_categories(self, cache, mock_CacheableSpiCase):
+        cache.get.return_value = None
+        mock_CacheableSpiCase.get.return_value = CacheableSpiCase('Fred',
+                                                                  1,
+                                                                  [SpiUserInfo('sock1', '1 January 2020'),
+                                                                   SpiUserInfo('sock2', '1 January 2020'),
+                                                                   SpiUserInfo('sock3', '1 January 2020')],
+                                                                  [])
+        wiki = NonCallableMock(Wiki)
+        wiki.validate_usernames.return_value = {}
+        wiki.category = MagicMock(Category)
+        wiki.category.reset_mock()
+        socks = get_sock_names(wiki, 'Fred')
+        self.assertEqual(wiki.category.call_args_list,
+                         [call('Wikipedia sockpuppets of Fred'),
+                          call('Suspected Wikipedia sockpuppets of Fred')])
+
+
+    @patch('spi.views.CacheableSpiCase')
+    @patch('spi.views.cache')
+    def test_get_sock_names_marks_sock_types(self, cache, mock_CacheableSpiCase):
+        cache.get.return_value = None
+        mock_CacheableSpiCase.get.return_value = CacheableSpiCase('Fred',
+                                                                  1,
+                                                                  [SpiUserInfo('sock1', '1 January 2020'),
+                                                                   SpiUserInfo('sock2', '1 January 2020'),
+                                                                   SpiUserInfo('sock3', '1 January 2020')],
+                                                                  [])
+        wiki = NonCallableMock(Wiki)
+        wiki.validate_usernames.return_value = {}
+        wiki.category = MagicMock(Category)
+        wiki.category().members.side_effect = [['User:sock1'],
+                                               ['User:sock2']]
+        wiki.category.reset_mock()
+        socks = get_sock_names(wiki, 'Fred')
+        self.assertEqual(wiki.category.call_args_list,
+                         [call('Wikipedia sockpuppets of Fred'),
+                          call('Suspected Wikipedia sockpuppets of Fred')])
+        self.assertCountEqual(list(socks),
+                              [ValidatedUser('sock1', '1 January 2020', True, SockType.KNOWN),
+                               ValidatedUser('sock2', '1 January 2020', True, SockType.SUSPECTED),
+                               ValidatedUser('sock3', '1 January 2020', True, SockType.NONE)])
--- spi/views.py
+++ spi/views.py
@@ -1,5 +1,7 @@
 from dataclasses import dataclass
+from enum import IntEnum
 import logging
+import re
 
 from django.core.cache import cache
 
@@ -9,11 +11,18 @@ from spi.spi_utils import CacheableSpiCase
 logger = logging.getLogger('spi.views')
 
 
+class SockType(IntEnum):
+    NONE = 0
+    SUSPECTED = 1
+    KNOWN = 2  # proven or confirmed
+
+
 @dataclass(frozen=True, order=True)
 class ValidatedUser:
     username: str
     date: str
     valid: bool
+    sock_type: SockType = SockType.NONE
 
 
 def get_sock_names(wiki, master_name):
@@ -30,13 +39,34 @@ def get_sock_names(wiki, master_name):
         # Need to work out cache invalidation
         usernames = [user_info.username for user_info in case.users]
         invalid_names = wiki.validate_usernames(usernames)
+        known_socks = _users_from_category(wiki, f'Wikipedia sockpuppets of {master_name}')
+        suspected_socks = _users_from_category(wiki, f'Suspected Wikipedia sockpuppets of {master_name}')
         users = []
         for user_info in case.users:
             name = user_info.username
             valid = name not in invalid_names
-            user = ValidatedUser(name, user_info.date, valid)
+            sock_type = _classify_sock_type(name, known_socks, suspected_socks)
+            user = ValidatedUser(name, user_info.date, valid, sock_type)
             if not valid:
                 logger.warning('invalid username (%s) in case "%s"', user, master_name)
             users.append(user)
         cache.set(key, users, 300)
     return users
+
+
+def _users_from_category(wiki, category_name):
+    name_list = []
+    pattern = re.compile(r'User:(.*)$')
+    for member in wiki.category(category_name).members():
+        m = pattern.match(member)
+        if m:
+            name_list.append(m[1])
+    return name_list
+
+
+def _classify_sock_type(name, known_socks, suspected_socks):
+    if name in known_socks:
+        return SockType.KNOWN
+    if name in suspected_socks:
+        return SockType.SUSPECTED
+    return SockType.NONE
