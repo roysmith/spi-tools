23befd67601197d47d419797ce8b01e4f858b710
Roy Smith <roy@panix.com>
2022-09-16 14:55:11 +0000

    Cleanup obsolete comment.

--- tools_app/settings.py
+++ tools_app/settings.py
@@ -35,9 +35,6 @@ m = re.match(r'.*/(?P<tool_name>[^/]*)/www/python/src', BASE_DIR)
 TOOL_NAME = m.group('tool_name') if m else 'unknown'
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('DJANGO_SECRET')
 
